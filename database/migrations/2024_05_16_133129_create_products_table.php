a12df20048d66eab9218a3409b6bcd27b7ecf27a
Naivians <madera.marvin98@gmail.com>
2024-05-31 19:46:21 +0800

    before update

--- database/migrations/2024_05_16_133129_create_products_table.php
+++ database/migrations/2024_05_16_133129_create_products_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('code');
             $table->string('description');
             $table->decimal('price', 10, 2);
-            $table->string('icon')->nullable();
         });
     }
 
